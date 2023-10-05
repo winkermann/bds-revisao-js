787e4eded2951649ea2352efab89288672a56571
Winkermann Meschke <willmeschke@gmail.com>
2023-10-05 13:51:04 -0300

    correction of files from json, operadores and string folders

--- revisao-js/operadores/script.js
+++ revisao-js/operadores/script.js
@@ -1,44 +1,40 @@
-console.log("Um string literal pode ficar entre aspas duplas");
-console.log('Um string literal pode ficar entre aspas simples');
-console.log(`Um string literal pode ficar entre crases`);
+console.log("Operadores aritméticos");
+console.log(`3 + 4 = ${3 + 4}`);
+console.log(`3 - 4 = ${3 - 4}`);
+console.log(`3 * 4 = ${3 * 4}`);
+console.log(`19 / 3 = ${19 / 5}`);
+console.log(`19 % 3 = ${19 % 5}`);
+console.log(`3 ** 4 = ${3 ** 4}`);
 
-console.log("Concatenação e interpolação");
-const num = 14.5;
-console.log("O valor do produto é " + num);
-console.log('O valor do produto é ' + num);
-console.log(`O valor do produto é ${num}`);
+console.log("Operadores comparativos");
+console.log(`10 > 5 = ${10 > 5}`);
+console.log(`10 < 5 = ${10 < 5}`);
+console.log(`10 >= 5 = ${10 >= 5}`);
+console.log(`10 <= 5 = ${10 <= 5}`);
 
-console.log("Conversão entre number e string");
-const str1 = num.toString();
-console.log(`Tipo de ${str1}: ${typeof str1}`);
+console.log("Operadores comparativos de igualdade");
+console.log(`"" == false: ${"" == false}`);
+console.log(`"" === false: ${"" === false}`);
+console.log(`"17" == 17: ${"17" == 17}`);
+console.log(`"17" === 17: ${"17" === 17}`);
 
-const str2 = num.toFixed(2);
-console.log(`Tipo de ${str2}: ${typeof str2}`);
+console.log("Operadores comparativos de igualdade");
+const x = "Maria";
+const y = "Maria";
+const a = new String("Maria");
+const b = new String("Maria");
+console.log(`const x = "Maria"`);
+console.log(`const y = "Maria"`);
+console.log(`const a = new String("Maria")`);
+console.log(`const b = new String("Maria"`);
+console.log(`x === y: ${x === y}`);
+console.log(`a === b: ${a === b}`);
+console.log(`typeof x: ${typeof x}`);
+console.log(`typeof a: ${typeof a}`);
 
-const num1 = Number("14.5");
-console.log(`Tipo de ${num1}: ${typeof num1}`);
-
-const num2 = parseInt("21", 10);
-console.log(`Tipo de ${num2}: ${typeof num2}`);
-
-const num3 = parseInt("21", 16);
-console.log(`Tipo de ${num3}: ${typeof num3}`);
-
-const num4 = parseFloat("21.34", 10);
-console.log(`Tipo de ${num4}: ${typeof num4}`);
-
-console.log("Funções de string");
-
-const str = "Maria Silva  ";
-console.log(`const str = "Maria Silva  "`);
-
-console.log(`toLowerCase: ${str.toLowerCase()}`);
-console.log(`toUpperCase: ${str.toUpperCase()}`);
-console.log(`charAt: ${str.charAt(3)}`);
-console.log(`replace (primeiro): ${str.replace("i", "$", )}`);
-console.log(`replace (todos): ${str.replace(/i/g, "$", )}`);
-console.log(`length: ${str.length}`);
-console.log(`indexOf: ${str.indexOf("i")}`);
-console.log(`lastIndexOf: ${str.lastIndexOf("i")}`);
-console.log(`subtring: ${str.substring(3, 8)}`);
-console.log(`trim: ${str.trim()}`);
+console.log("Operadores lógicos");
+console.log(`(10 > 5) && (10 > 20): ${(10 > 5) && (10 > 20)}`);
+console.log(`(10 > 5) && (10 < 20): ${(10 > 5) && (10 < 20)}`);
+console.log(`(10 > 5) || (10 > 20): ${(10 > 5) || (10 > 20)}`);
+console.log(`(10 < 5) || (10 > 20): ${(10 > 5) && (10 > 20)}`);
+console.log(`!(10 > 5): ${!(10 > 5)}`);
