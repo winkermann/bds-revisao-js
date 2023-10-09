e7f2d025b48e9d7e6fc20d6fdf723f19549dcf38
Winkermann Meschke <willmeschke@gmail.com>
2023-10-09 19:08:33 -0300

    add dom selectors review

---
+++ revisao-js/DOM-selecao-elementos/script.js
@@ -0,0 +1,18 @@
+const item1 = document.querySelector("li")
+const items = document.querySelectorAll("li")
+const card = document.querySelector(".card")
+
+
+//em desuso
+const item2 = document.getElementById("item2");
+const cards = document.getElementsByClassName("card")
+const cards2 = document.getElementsByTagName("div")
+
+console.log(item1)
+console.log(items)
+console.log(card)
+
+console.log("- - - - - - - - - - - - - -")
+console.log(item2)
+console.log(cards)
+console.log(cards2)
