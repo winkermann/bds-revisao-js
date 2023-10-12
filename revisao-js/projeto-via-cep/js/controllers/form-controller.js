b0183cb6a2ed8ee55b7dd51b5bafe2321bae66bf
Winkermann Meschke <willmeschke@gmail.com>
2023-10-11 21:35:25 -0300

    created form controller

---
+++ revisao-js/projeto-via-cep/js/controllers/form-controller.js
@@ -0,0 +1,32 @@
+//responsável por controlar o formulário
+
+//função construtora. Será responsável por guardar as informações relevantes do módulo form-controller.
+function State() {
+  this.btnSave = null;
+  this.btnClear = null;
+
+  this.inputCep = null;
+  this.inputStreet = null;
+  this.inputNumber = null;
+  this.inputCity = null;
+
+  this.errorCep = null;
+  this.errorNumber = null;
+}
+
+const state = new State()
+
+export function init() {
+  state.inputCep = document.forms.newAddress.cep; 
+  state.inputStreet = document.forms.newAddress.street; 
+  state.inputNumber = document.forms.newAddress.number; 
+  state.inputCity = document.forms.newAddress.city; 
+
+  state.btnSave = document.forms.newAddress.btnSave;
+  state.btnClear = document.forms.newAddress.btnClear;
+  
+  state.errorCep = document.querySelector('[data-error="cep"]');
+  state.errorNumber = document.querySelector('[data-error="number"]');
+  
+  console.log(state);
+}
